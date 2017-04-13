ef211058505c07dd1311a3fdb357ea26d87500fe
Mike H <tapeworm09@hotmail.com>
2017-04-13 14:58:31 +0200

    working tracker i think

--- myTeam.py
+++ myTeam.py
@@ -56,19 +56,22 @@ class MCTS:
     
 class EnemyTracker: ######### TODO: Keep track of eaten food, and assign it to most likely agent
     # counter object for keeping track of enemy positions
-    # self.tracker : dict of counters
+    # self.tracker : list of counters
     # self.enemy_idxs = [];
 
     def init(self, gameState, isRed):    
         self.red = isRed;
         self.first_update = True;
         self.enemy_idxs = [];
+        self.enemy_edge = [];
         if(isRed): # enemy blue
             self.enemy_idxs = gameState.getBlueTeamIndices();
             self.old_food_state = gameState.getRedFood();
+            x_edge = gameState.data.layout.width / 2;
         else:
             self.enemy_idxs = gameState.getRedTeamIndices();
             self.old_food_state = gameState.getBlueFood();
+            x_edge = gameState.data.layout.width / 2 - 1;
 
         
         
@@ -76,7 +79,7 @@ class EnemyTracker: ######### TODO: Keep track of eaten food, and assign it to m
         all_idxs.extend(gameState.getBlueTeamIndices());
         all_idxs.sort();
         self.tracker = [None]*len(all_idxs);
-        
+        self.carrying_food = [0]*len(self.enemy_idxs);
 
         for i in all_idxs:
             if(i in self.enemy_idxs):                   
@@ -102,59 +105,139 @@ class EnemyTracker: ######### TODO: Keep track of eaten food, and assign it to m
 
         self.old_food_state = new_food_state;
 
+        temp_trackers = {};
         measured_dists = gameState.getAgentDistances();
         for i in self.enemy_idxs: #{
             exact_pos = gameState.getAgentPosition(i);
             if(exact_pos != None): #{
-                self.tracker[i] = Counter();
-                self.tracker[i][exact_pos] = 1.0;
+                temp_trackers[i] = Counter();
+                temp_trackers[i][exact_pos] = 1.0;
             else:
-                temp_tracker = Counter();
+                temp_trackers[i] = Counter();
                 for key,value in self.tracker[i].iteritems(): #{
                     if(value == 0.0):
                         continue;
                     
                     if(my_index == 0 and self.first_update):
                         self.first_update = False;    
-                        temp_tracker[key] = value;
+                        temp_trackers[i][key] = value;
                         continue;
 
                     if((my_index - 1) % gameState.getNumAgents() == i):   #if this agent moved last turn, update his pos             
-                        p_move = 0;
+                        p_move = np.zeros(5);
+                        k = 0;
                         for direction,_ in Actions._directions.iteritems(): #{
                             pos = Actions.getSuccessor(key,direction);
                             if(not gameState.hasWall(int(pos[0]),int(pos[1]))):
-                                p_move += 1;
+                                p_move[k] += 1;
+                            if(pos not in self.tracker[i]):                        
+                                p_move[k] *= 2;
+                            if(direction == Directions.STOP):
+                                p_move[k] /= 2;
+                                
+                            k += 1;
                         #}
 
-                        p_move = 1.0 / p_move;
+                        p_move = p_move / np.sum(p_move);
 
+                        k = 0;
                         for direction,_ in Actions._directions.iteritems(): #{
                             pos = Actions.getSuccessor(key,direction);
                             if(not gameState.hasWall(int(pos[0]),int(pos[1]))):
-                                temp_tracker[pos] += p_move * value;
+                                temp_trackers[i][pos] += p_move[k] * value;
+                            k += 1;
                         #}
                     else: #if this agent did not move last turn, pretend he moved using action STOP   
-                        temp_tracker[key] = value;
+                        temp_trackers[i][key] = value;
                     #}
                 #}
                 
-                for key,value in temp_tracker.iteritems(): #{
+                for key,value in temp_trackers[i].iteritems(): #{
                     true_dist = manhattanDistance(key,gameState.getAgentPosition(my_index));
-                    temp_tracker[key] = value * gameState.getDistanceProb(true_dist, measured_dists[i]);
-                    if(key in eaten_food):
-                        temp_tracker[key] = 1;
+                    if(true_dist <= 5):
+                        temp_trackers[i][key] = 0.0;
+                    else:
+                        temp_trackers[i][key] = value * gameState.getDistanceProb(true_dist, measured_dists[i]);
+                    #if(key in eaten_food):
+                        #temp_trackers[i][key] = 1;
                 #}
-                self.tracker[i] = deepcopy(temp_tracker);
-                self.tracker[i].normalize();
+                
             #}
         #}
+                
+        print(self.carrying_food );
+        for key in eaten_food:
+            if(temp_trackers[self.enemy_idxs[0]][key] > temp_trackers[self.enemy_idxs[1]][key]):
+                temp_trackers[self.enemy_idxs[0]][key] = 2;
+                self.carrying_food[self.enemy_idxs[0]] += 1;
+            else:
+                temp_trackers[self.enemy_idxs[1]][key] = 2;
+                self.carrying_food[self.enemy_idxs[1]] += 1;
+
+        for key, _ in temp_trackers.iteritems():
+            self.tracker[key] = deepcopy(temp_trackers[key]);
+            self.tracker[key].normalize();
+
+        scorediff = abs(gameState.data.scoreChange);
+        if(scorediff > 0):
+            if(self.carrying_food[self.enemy_idxs[0]] != 0 and self.carrying_food[self.enemy_idxs[1]] == 0):
+                for key, _ in self.tracker[self.enemy_idxs[0]].iteritems():
+                    if(key[0] not in self.enemy_edge):
+                        self.tracker[self.enemy_idxs[0]][key] = 0.0;
+                self.carrying_food[self.enemy_idxs[0]] = 0;
+            elif(self.carrying_food[self.enemy_idxs[0]] == 0 and self.carrying_food[self.enemy_idxs[1]] != 0):
+                 for key, _ in self.tracker[self.enemy_idxs[1]].iteritems():
+                    if(key[0] not in self.enemy_edge):
+                        self.tracker[self.enemy_idxs[1]][key] = 0.0;
+                 self.carrying_food[self.enemy_idxs[1]] = 0;
+            elif(self.carrying_food[self.enemy_idxs[0]] != 0 and self.carrying_food[self.enemy_idxs[1]] != 0):
+                most_likely_enemy = self.enemy_idxs[0];
+                less_likely_enemy = self.enemy_idxs[1];
+                max_prob = -1.0;
+                for key, _ in self.tracker[self.enemy_idxs[1]].iteritems():
+                    if(key[0] in self.enemy_edge):
+                        if(max_prob < self.tracker[self.enemy_idxs[1]][key]):
+                            max_prob = self.tracker[self.enemy_idxs[1]][key];
+                            most_likely_enemy = self.enemy_idxs[1];
+                            less_likely_enemy = self.enemy_idxs[0];
+                for key, _ in self.tracker[self.enemy_idxs[0]].iteritems():
+                    if(key[0] in self.enemy_edge):
+                        if(max_prob < self.tracker[self.enemy_idxs[0]][key]):
+                            max_prob = self.tracker[self.enemy_idxs[0]][key];
+                            most_likely_enemy = self.enemy_idxs[0];
+                            less_likely_enemy = self.enemy_idxs[1];
+                
+                diff = self.carrying_food[most_likely_enemy] - scorediff;       
+                if(diff < 0):
+                    self.carrying_food[most_likely_enemy] = 0;
+                    self.carrying_food[less_likely_enemy] = max(0,self.carrying_food[less_likely_enemy] - diff);
+                elif(diff > 0):
+                    self.carrying_food[most_likely_enemy] = 0;
+                    self.carrying_food[less_likely_enemy] += diff;
+                else:
+                    self.carrying_food[most_likely_enemy] = 0;
+
+                for key, _ in self.tracker[most_likely_enemy].iteritems():
+                    if(key[0] not in self.enemy_edge):
+                        self.tracker[self.enemy_idxs[1]][key] = 0.0;
+                   
+                for key, _ in trackers.iteritems():
+                    self.tracker[key].normalize();
     #}
 
+    def estimate_enemy_pos(self, enemyIndex):
+        max_prob = -1;
+        pos = 0;
+        for key, value in self.tracker[enemyIndex].iteritems():
+            if(value > max_prob):
+                max_prob = deepcopy(value);
+                pos = deepcopy(key);
+        return pos, max_prob;
 
     def update_eaten_agent(self,gameState,index_eaten):
         self.tracker[index_eaten] = Counter();
         self.tracker[index_eaten][gameState.getInitialAgentPosition(index_eaten)] = 1.0;
+        self.carrying_food[index_eaten] = 0;
 
 
 
@@ -223,6 +306,15 @@ class DummyAgent(CaptureAgent):
     tracker.init(gameState,self.red);
 	
 
+    self.first = False;    
+
+    if(self.red): # enemy blue
+        self.enemy_idxs = gameState.getBlueTeamIndices();
+        self.enemy_edge = gameState.data.layout.width / 2;
+    else:
+        self.enemy_idxs = gameState.getRedTeamIndices();
+        self.enemy_edge = gameState.data.layout.width / 2 - 1;
+
 
     #state = gameState;
     #turn = 0;
@@ -249,6 +341,9 @@ class DummyAgent(CaptureAgent):
     You should change this in your own agent.
     '''
 
+    my_index = self.index;
+    my_pos = gameState.getAgentPosition(self.index);
+
     #n_agents = gameState.getNumAgents();
 
     #print (gameState)
@@ -263,8 +358,29 @@ class DummyAgent(CaptureAgent):
     tracker.update(gameState,self.index);    
     self.displayDistributionsOverPositions(tracker.tracker);
 
+    enemy0_pos, enemy0_prob = tracker.estimate_enemy_pos(self.enemy_idxs[0]);
+    enemy1_pos, enemy1_prob = tracker.estimate_enemy_pos(self.enemy_idxs[1]);
+    
+    enemy_positions = [enemy0_pos, enemy1_pos];
 
-    #print (gameState)
+    #print (gameState.getScore())
+
+    #print(self.index)
+    #if(self.index == 2 and not self.first):
+    #    self.first = True;
+    #    state = gameState;
+    #    turn = 2;
+
+    #    act = state.getLegalActions(turn);
+    #    state = state.generateSuccessor(turn,np.random.choice(act));
+    #    turn = (turn + 1) % 4;
+        
+    #    print(state.data.agentStates[3])
+
+    # need to generate a AgentState
+    # need to generate a Configuration
+    # for each enemy agent, with the current belief
+
 
     ##if gameState.getAgentDistances() != None:    
     #n = len(gameState.getAgentDistances());
@@ -280,9 +396,14 @@ class DummyAgent(CaptureAgent):
     #    myclass.x += 1;
 
     #print(gameState)
-    time.sleep(0.01)
-    
-    return random.choice(actions)
+    #time.sleep(0.01)
+    my_action = random.choice(actions);
+    if(Actions.getSuccessor(my_pos,my_action) == enemy0_pos):
+        tracker.update_eaten_agent(gameState,self.enemy_idxs[0]);
+    if(Actions.getSuccessor(my_pos,my_action) == enemy1_pos):
+        tracker.update_eaten_agent(gameState,self.enemy_idxs[1]);
+
+    return my_action;
 
 
 
